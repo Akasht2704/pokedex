35a777c78c081e24f3848502cfe94f6bb032b1c3
Akasht2704 <akasht2704@gmail.com>
2024-02-11 22:58:11 +0530

    css and button added

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -18,10 +18,10 @@ function PokemonList() {
 
         // iterating over the array of pokemons, and using their url, to create an array of promises
         // that will download those 20 pokemons
-        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+        const pokemonResultPromise = pokemonResults.map((pokemon)=> axios.get(pokemon.url));
 
         // passing that promise array to axios.all
-        const pokemonData = await axios.all(pokemonResultPromise); // array of 20 pokemon detailed data
+        const pokemonData = await axios.all(pokemonResultPromise); // array of 20 pokemon detaileddata
         console.log(pokemonData); 
 
         // now iterate on the data of each pokemon, and extract id, name, image, types
@@ -46,10 +46,15 @@ function PokemonList() {
 
     return (
         <div className="pokemon-list-wrapper">
-            <div>Pokemon List</div>
-            {(isLoading) ? 'Loading....' : 
-                pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)
-            }
+            <div className="pokemon-wrapper">
+                {(isLoading) ? 'Loading....' : 
+                    pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)
+                }
+            </div>
+            <div className="controls">
+                <button>Prev</button>
+                <button>Next</button>
+            </div>
         </div>
     )
 }
